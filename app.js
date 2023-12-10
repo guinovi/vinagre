11b0532365169fc9cdec148b25bebd8cd2aadfb7
Guillermo Novillo <gui.novillo@gmail.com>
2023-12-10 17:06:32 -0300

    carrusel Move

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 const navWines = document.getElementById('navWines');
 const conteinPrimary = document.getElementById('conteinPrimary');
+const carrucel = document.getElementById('carrucel');
+
 
 
 const fetchData = async(apiUrl) =>{
@@ -23,24 +25,35 @@ const fetchData = async(apiUrl) =>{
 
 
 // DATA -->api
-const datos = (data) =>{
-    const template = document.getElementById("template").content
+const datos = (data) => {
+    const template = document.getElementById("template").content;
     const fragment = document.createDocumentFragment();
-    
+    const fragmentDos = document.createDocumentFragment();
+
     data.forEach(element => {
         const point = element.rating.average;
-
         const clone = template.cloneNode(true);
+        const cloneDos = template.cloneNode(true);
+        // clon
         clone.getElementById('cardImg').setAttribute("src", element.image);
         clone.getElementById('cardTitle').textContent = element.wine;
         clone.getElementById('cardVineria').textContent = element.winery;
         clone.getElementById('cardFrom').textContent = `Origen: ${element.location}`;
         clone.getElementById('cardRating').textContent = `Rating: ${point}`;
-
+        // Clon 2 --> carrucel
+        cloneDos.getElementById('cardImg').setAttribute("src", element.image);
+        cloneDos.getElementById('cardTitle').textContent = element.wine;
+        cloneDos.getElementById('cardVineria').textContent = element.winery;
+        cloneDos.getElementById('cardFrom').textContent = `Origen: ${element.location}`;
+        cloneDos.getElementById('cardRating').textContent = `Rating: ${point}`;
+        //
         fragment.appendChild(clone);
+        fragmentDos.appendChild(cloneDos);
     });
+
     conteinPrimary.appendChild(fragment);
-}   
+    carrucel.appendChild(fragmentDos);
+}  
 
 //Cargando....  
 const loading = (estado) =>{
@@ -56,7 +69,7 @@ const loading = (estado) =>{
 }
 
 
-
+//NAV SHOP
 navWines.addEventListener('click', (e) => {
     e.preventDefault();
     conteinPrimary.innerHTML = ""//boora contenido
@@ -67,3 +80,35 @@ navWines.addEventListener('click', (e) => {
 });
 
 
+
+//index - CARRUSEL
+function autoScroll() {
+    let limiteScroll = carrucel.scrollWidth - carrucel.clientWidth;
+    let avanceScroll = 1;
+    setInterval(() => {        
+        carrucel.scrollLeft = carrucel.scrollLeft + avanceScroll
+        // if ( limiteScroll === carrucel.scrollLeft){
+        //     avanceScroll = -1
+        // } else if (carrucel.scrollLeft === 0){
+        //     avanceScroll =  1 
+        // }
+    }, 50);
+} 
+autoScroll()
+
+
+/* let maxScrollLeft = carrucel.scrollWidth - carrucel.clientWidth;
+let intervalo = null;
+let step = 1;
+const start = () => {
+  intervalo = setInterval(function () {
+    carrucel.scrollLeft = carrucel.scrollLeft + step;
+    if (carrucel.scrollLeft === maxScrollLeft) {
+      step = step * -1;
+    } else if (carrucel.scrollLeft === 0) {
+      step = step * -1;
+    }
+  }, 10);
+};
+
+start(); */
